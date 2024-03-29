cabe3b07c98a480cb3264918b47ba4be05de0153
居戎氏 <chen.sst@gmail.com>
2024-03-29 19:38:25 +0800

    format code

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,11 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::ffi::{CStr};
-    use std::os::raw::{c_int};
+    use std::ffi::CStr;
+    use std::os::raw::c_int;
 
     fn round_up(x: usize, multiple: usize) -> usize {
         let remainder = x % multiple;
@@ -26,8 +24,11 @@ mod tests {
             let rime_api = rime_get_api();
             assert_eq!(
                 std::mem::size_of::<RimeApi>(),
-                round_up((*rime_api).data_size as usize,
-                         std::mem::align_of::<RimeApi>()));
+                round_up(
+                    (*rime_api).data_size as usize,
+                    std::mem::align_of::<RimeApi>()
+                )
+            );
         }
     }
 
@@ -38,13 +39,15 @@ mod tests {
 
             let setup = (*rime_api).setup;
             assert!(setup.is_some());
-            let mut test_traits : RimeTraits = std::mem::zeroed();
+            let mut test_traits: RimeTraits = std::mem::zeroed();
             test_traits.data_size = std::mem::size_of::<RimeTraits>() as c_int;
             test_traits.shared_data_dir = CStr::from_bytes_with_nul(b".\0").unwrap().as_ptr();
             test_traits.user_data_dir = CStr::from_bytes_with_nul(b".\0").unwrap().as_ptr();
             test_traits.distribution_name = CStr::from_bytes_with_nul(b"test\0").unwrap().as_ptr();
-            test_traits.distribution_code_name = CStr::from_bytes_with_nul(b"test\0").unwrap().as_ptr();
-            test_traits.distribution_version = CStr::from_bytes_with_nul(b"0.1\0").unwrap().as_ptr();
+            test_traits.distribution_code_name =
+                CStr::from_bytes_with_nul(b"test\0").unwrap().as_ptr();
+            test_traits.distribution_version =
+                CStr::from_bytes_with_nul(b"0.1\0").unwrap().as_ptr();
             test_traits.app_name = CStr::from_bytes_with_nul(b"test\0").unwrap().as_ptr();
             test_traits.modules = std::ptr::null_mut();
             let setup = setup.unwrap();
@@ -54,19 +57,23 @@ mod tests {
             assert!(find_module.is_some());
             let find_module = find_module.unwrap();
             {
-                let core_module = find_module(CStr::from_bytes_with_nul(b"core\0").unwrap().as_ptr());
+                let core_module =
+                    find_module(CStr::from_bytes_with_nul(b"core\0").unwrap().as_ptr());
                 assert!(!core_module.is_null());
             }
             {
-                let dict_module = find_module(CStr::from_bytes_with_nul(b"dict\0").unwrap().as_ptr());
+                let dict_module =
+                    find_module(CStr::from_bytes_with_nul(b"dict\0").unwrap().as_ptr());
                 assert!(!dict_module.is_null());
             }
             {
-                let gears_module = find_module(CStr::from_bytes_with_nul(b"gears\0").unwrap().as_ptr());
+                let gears_module =
+                    find_module(CStr::from_bytes_with_nul(b"gears\0").unwrap().as_ptr());
                 assert!(!gears_module.is_null());
             }
             {
-                let levers_module = find_module(CStr::from_bytes_with_nul(b"levers\0").unwrap().as_ptr());
+                let levers_module =
+                    find_module(CStr::from_bytes_with_nul(b"levers\0").unwrap().as_ptr());
                 assert!(!levers_module.is_null());
             }
         }
